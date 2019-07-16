ab83b4baf5087d9edbed47802ad153a79d3c92b7
Fachri Ikhsan <fachri.ikh1213@gmail.com>
2019-07-16 09:56:13 +0700

    Pengantar pasar muamalah

--- assets/js/lunrFeed.js
+++ assets/js/lunrFeed.js
@@ -2,10 +2,19 @@ var documents = [
 	
 	
 		{
+			"title": "Pengantar Pasar Muamalah",
+			"content": "Sistem ekonomi dalam Islam tidak jauh berbeda dengan sistem ekonomi lain. Hanya saja lebih sederhana, adil, bebas dan transparan. Sederhana untuk dipahami dan dilakukan, adil dalam harga dan takaran, bebas dari yang haram dan bebas dari kezhaliman, serta transparan dalam bertransaksi.\n\nTidak mustahil bagi ummat Islam untuk mendirikan sistem ekonomi se “indah” ini. Karena dalam Islam, ekonomi harus selalu sepaket dan berpasangan dengan sosial.  Peran sosial disini berfungsi untuk membangun dan menjaga ke ridhoan, bukan saja ridho makhluk tapi juga ridho Allah  ‘Azza wa Jalla. Apabila Allah ridho maka seluruh pihak yang terlibat transaksi akan mendapatkan keberkahan. Keberkahan itulah yang seharusnya dikejar, bukan besarnya laba atau mewahnya harta.\n\nSinergi antara ekonomi dan sosial menyebabkan pasar menjadi elemen sentral yang wajib ada dalam kehidupan ummat. Pasar menjadi pusat serta tolak ukur kekuatan dan kesehatan ekonomi dan sosial ummat. Pasar yang jujur, adil, dan bebas dari kezhaliman dapat memperbaiki dan meningkatkan perekonomian ummat serta mempererat ikatan ukhuwah jamaah Islamiyah. Untuk menciptakan kondisi pasar seperti itu, tentu diperlukan suatu ketentuan, aturan, bahkan hukum untuk menegakkan dan menjaga nilai-nilai luhur tersebut, dan tidak ada satupun di seluruh semesta ini suatu hukum yang paling kuat, paling mulia, dan paling sempurna selain hukum Allah ‘Azza wa Jalla.\n\nAhad 26 Syawal, bertepatan dengan tanggal 30 Juni, kembali diadakan Pasar Muamalah oleh komunitas pedagang disekitar Baitul Mal Nusantara Depok, tepatnya di jalan Tanah Baru No 23, Beji. Tidak seperti objek ekonomi lain yang juga menggunakan tajuk ‘Muamalah’, Pasar Muamalah kembali menerapkan hampir secara utuh sistem pasar yang diterapkan pada masa Rasulullah SAW. Tidak ada sewa, semua pedagang yang ingin berjualan diperbolehkan menggelar barang daganganya di pasar tersebut tanpa biaya. Meja pun sudah disiapkan oleh panitia, siapa cepat dia dapat. Selain tanpa sewa, pajak juga tidak berlaku disini. Para pedagang dapat berjualan dengan tenang karena tidak ada biaya tersembunyi atau pemungutan dalam bentuk apapun terhadap usaha mereka.\n\nMenerapkan pasar tanpa sewa dan bebas pajak memang merupakan sebuah langkah besar dan salah satu poin pokok dari menerapkan sistem ekonomi yang benar-benar syar’i, namun bukan satu-satunya. Misi pokok dan daya tarik paling utama dari Pasar Muamalah ini adalah kembalinya uang yang haq, yang nyata, yang telah disebutkan dalam Al-Qur’an dan As Sunnah yaitu Dinar dan Dirham.\n\nDengan mulai berkembangnya pasar muammalah, masyarakat telah berikhtiar untuk menjalankan prinsip ekonomi syariah yang sesuai dengan tuntunan agama. Skala kecil dan keterbatasan lahan tak menjadi soal bagi para pihak yang berpartisipasi. Tujuan utama mereka adalah  memasyarakatkan prinsip jual beli syariah melalui interaksi secara langsung dengan masyarakat kecil dan menengah. Pasar muammalah pada hakekatnya adalah sebuah sistem yang Allah rancang untuk melindungi harta setiap hamba-Nya. Oleh karena itu, sudah selayaknya pasar muammalah mendapat dukungan dari berbagai lapisan masyarakat. Tentu saja perlu waktu untuk menggalang kesadaran, dukungan dan partisipasi masyarakat, namun hal tersebut sangat mungkin dilakukan selama ikhtiar tidak berhenti. Allah tidak akan merubah suatu kaum jika mereka tidak berusaha. Akhir kata, perjuangan untuk memasyarakatkan pasar muammalah masih panjang dan akan menjadi sarana untuk menyatukan ummat dalam bahu-membahu menegakkan keadilan dan kesejahteraan bersama.\n",
+			"labels": [""],
+			"id": 0
+		}
+	,
+	
+	
+		{
 			"title": "F.A.Q",
 			"content": "Frequently Asked Questions\n    1. Kenapa namanya Nazhifa?\nKami memilih nama Nazhifa karena maknanya sesuai dengan tujuan utama dan filosofis kami. Nazhifa diambil dari bahasa Arab yang bermakna “bersih”. Kami berharap Nazhifa bisa menjadi langkah pertama kami dalam memulai dan beristiqomah dalam hidup bersih sesuai prinsip Islami.\n\n2. Apa tujuan awal berdirinya Nazhifa? Nazhifa didirikan dengan tujuan untuk menyediakan berbagai keperluan Islami dan bermanfaat bagi mereka yang ingin memulai hidup bersih sesuai tuntunan Islam serta membantu para pembelajar dalam mendapatkan sumber ilmu pengetahuan yang terjangkau.\n \n3. Kapan Nazhifa berdiri? Nazhifa berdiri pada bulan Desember tahun 2018.\n\n4. Dimanakah Nazhifa di dirikan? Nazhifa didirikan di Jakarta Timur. \n\n5. Bolehkah menyebarkan link gratis yang ada di Nazhifa? Tentu saja. Kami mempersilahkan para pembaca untuk membagikan dan menyebarkan tautan sumber imu pengetahuan gratis yang kami sebarkan di website Nazhifa. Dengan berbagi ilmu, para pembaca dapat saling membantu dan menambah pahala.\n\n6. Apa saja produk Nazhifa? Untuk saat ini, produk Nazhifa antara lain buku, siwak, khimar syari, dan madu. Kami berharap untuk kedepannya dapat menambah koleksi produk dan juga jasa kami.\n\n7. Kok murah banget? Amanah gak nih? Jangan-jangan nipu ya? Insya Allah, kami amanah. Tentu dalam bekerja pasti kami tidak luput dari kesalahan. Namun, kami akan mengkomunikasikanya se transparan mungkin sehingga tidak terjadi fitnah demi menjamin kelancaran dan kehalalan muammalah kami. Perlu digaris bawahi kami selalu memastikan kualitas produk-produk kami. \n\n8. Selain toko dan situs free resource, apa saja proyek Nazhifa? Nazhifa sedang berupaya untuk berpartisipasi dalam pasar muammalah syariah untuk membangkitkan transaksi menggunakan dinar dirham. Nazhifa juga memiliki proyek media dan lembaga pendidikan nantinya.\n\n\n9. Bagaimana prinsip muammalah Nahifa? Kami melayani pembelian langsung dan COD di daerah Jakarta Timur. Kami juga melayani pembelian online melalui WA, Instagram, Tokopedia, dan Bukalapak. Kami juga sedang berupaya mempersiapkan diri untuk berpartisipasi dalam muammalah menggunakan dinar dirham.\n\n10. Bagaimana kualitas barang? Kami selalu memastikan kualitas barang dalam keadaan baik dan baru. Kami hanya menjual dan mengirim barang dengan kualitas baik kepada pembeli. Apabila ada kendala dan informasi penting, kami akan selalu mengkomunikasikannya kepada pembeli.\n\n11. Adakah sistem refund? Ya, kami memiliki sistem refund bagi pembeli yang tidak menerima barang pesanan yang telah dibayar ataupun menerima produk yang rusak.\n\n12. Jika ingin kerja sama dengan Nazhifa, bagaimana cara menghubunginya? Kami dapat dihubungi melalui whatsapp dengan nomor 082113935183 / 08226209400.\n\n\n",
 			"labels": [""],
-			"id": 0
+			"id": 1
 		}
 	,
 	
@@ -14,7 +23,7 @@ var documents = [
 			"title": "Selamat Datang di Nazhifa!",
 			"content": "Selamat Datang dan terimakasih sudah berkenan untuk mampir di situs resmi Nazhifa. Kemungkinan besar Anda menemukan situs ini karena Anda mengklik link tautan dari sosial media kami, atau ketika Anda sedang berselancar menjelajahi internet mencari situs yang membagikan resource gratis. Yang manapun yang Anda lakukan, Anda tiba di tempat yang tepat!\n\nSesuai dengan nama Nazhifa yang memiliki arti “bersih”, Nazhifa dibangun dengan misi untuk membantu masyarakat yang ingin menjalani hidup bersih dengan menyediakan informasi dan berbagai kebutuhan islami. Langkah awal kami dalam mewujudkan hal tersebut yaitu dengan membagikan tautan-tautan sumber ilmu pengetahuan gratis, menjual buku-buku dan busana muslim serta produk herbal. Secara bertahap, kami akan mengembangkan diri dengan berproses menjadi sumber rujukan bagi ilmu pengetahuan. Kami juga akan meningkatkan kinerja kami dalam misi penyebaran ilmu pengetahuan dan penyedia kebutuhan sehari-hari.\n\nLangkah selanjutnya, tentu saja kami akan membawa misi kami ke tahap yang lebih luas dan lebih serius, sebagai upaya dalam mengembalikan peradaban Islam lewat ekonomi dan sosial. Tidak menutup kemungkinan kami akan bekerjasama dengan lembaga atau organisasi terkait baik dalam skala kecil maupun besar.\n\nKami meyakini bahwa informasi dan ilmu pengetahuan merupakan kebutuhan universal. Sayangnya, saat ini ilmu pengetahuan masih dijadikan produk ekslusif yang memiliki akses terbatas dan memerlukan biaya mahal. Tak semua orang bisa mengakses meski dengan dukungan teknologi. Kami optimis kami akan mampu mencapai misi yang kami tetapkan.\n\nAkhir kata, selamat mengeksplorasi berbagai tautan referensi terkait berbagai disiplin ilmu di Nazhifa. Yuk, cari informasi yang kamu butuhkan lalu akses sepuasmu. Jelajahi sumbernya, pelajari ilmunya, bagikan manfaatnya!\n",
 			"labels": [""],
-			"id": 1
+			"id": 2
 		}
 	,
 	
@@ -23,7 +32,7 @@ var documents = [
 			"title": "Halo Dunia!",
 			"content": "You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated.\n\nTo add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.\n\nJekyll also offers powerful support for code snippets:\n\ndef print_hi(name)\n  puts \"Hi, #{name}\"\nend\nprint_hi('Tom')\n#=&gt; prints 'Hi, Tom' to STDOUT.\n\nCheck out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.\n\n",
 			"labels": ["islam"],
-			"id": 2
+			"id": 3
 		}
 	,
 	
@@ -32,7 +41,7 @@ var documents = [
 			"title": "Ada apa sih!",
 			"content": "You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated.\n\nTo add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.\n\nJekyll also offers powerful support for code snippets:\n\ndef print_hi(name)\n  puts \"Hi, #{name}\"\nend\nprint_hi('Tom')\n#=&gt; prints 'Hi, Tom' to STDOUT.\n\nCheck out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.\n\n",
 			"labels": ["islam"],
-			"id": 3
+			"id": 4
 		}
 	,
 	
@@ -41,7 +50,7 @@ var documents = [
 			"title": "Welcome to Jekyll!",
 			"content": "You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated.\n\nTo add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.\n\nJekyll also offers powerful support for code snippets:\n\ndef print_hi(name)\n  puts \"Hi, #{name}\"\nend\nprint_hi('Tom')\n#=&gt; prints 'Hi, Tom' to STDOUT.\n\nCheck out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.\n\n",
 			"labels": ["jekyll","islam"],
-			"id": 4
+			"id": 5
 		}
 	
 	
@@ -60,6 +69,15 @@ var idx = lunr(function() {
 })
 
 var store = [
+	
+		{
+			'title': "Pengantar Pasar Muamalah",
+			'link': "/2019/07/16/pengantar-pasar-muamalah.html",
+			'date': "July 16, 2019",
+			'labels': [""],
+			'excerpt': "Sistem ekonomi dalam Islam tidak jauh berbeda dengan sistem ekonomi lain. Hanya saja lebih sederhana, adil, bebas dan transparan. Sederhana..."
+		}
+		,
 	
 		{
 			'title': "F.A.Q",
